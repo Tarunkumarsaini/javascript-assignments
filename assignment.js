ded3ad7d59a5a0b9e28c654ed3bc4bc4c05383d1
Tarunkumarsaini <123304632+Tarunkumarsaini@users.noreply.github.com>
2023-03-23 15:10:18 +0530

    update

--- assignment.js
+++ assignment.js
@@ -7,19 +7,8 @@ const members = [
     {name: 'Swpril Ahuja', age: 45},
     {name: 'Yogesh Khatri', age: 51}
   ];
-  let newarr = members.map(function(a){
-    return a.name;
+  const firstNames = members.map((a) => {
+    const firstNames = a.name.split(" ")[0];
+    return firstNames;
   });
-function mysort()
-{
-    for( let i = 0;i<newarr.length;i++)
-    {
-    let st = newarr[i].toString();
-    let new1 = st.split(" ");
-    let new2 = new1[0];
-    let arr = [];
-    arr[i] = new2;
-    console.log(arr);
-    } 
-}
- mysort();
+  console.log(firstNames);
